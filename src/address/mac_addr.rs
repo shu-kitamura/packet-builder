99346377abfb4c4bde6420a163a04a130e2d03c3
shu-kitamura <shusei3316@yahoo.co.jp>
2025-07-25 00:06:33 +0900

    use core::net::Ipv4Addr (#8)
    * change: Ipv4Addr, MacAddr
    
    - add to_bytes()
    - remove fmt() in MacAddr
    
    Signed-off-by: shu-kitamura <shusei3316@yahoo.co.jp>
    
    * delete Ipv4Addr
    
    Ipv4Addr is in core::net, so I removed my implementation.
    The function to convert Ipv4Addr to [u8;4] was named `octets`, so MacAddr was renamed accordingly.
    
    * change test_to_bytes -> test_octets
    
    ---------
    
    Signed-off-by: shu-kitamura <shusei3316@yahoo.co.jp>

--- src/address/mac_addr.rs
+++ src/address/mac_addr.rs
@@ -1,5 +1,3 @@
-use core::fmt;
-
 const MULTICAST_BIT: u8 = 0x01;
 const LOCAL_BIT: u8 = 0x02;
 
@@ -40,21 +38,9 @@ impl MacAddr {
     }
 }
 
-impl fmt::Display for MacAddr {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "{:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}",
-            self.0, self.1, self.2, self.3, self.4, self.5
-        )
-    }
-}
-
 #[cfg(test)]
 mod tests {
-    extern crate alloc;
     use super::*;
-    use alloc::string::ToString;
 
     #[test]
     fn test_new() {
@@ -121,13 +107,5 @@ mod tests {
 
         let mac = MacAddr::new(0x02, 0x02, 0x03, 0x04, 0x05, 0x06);
         assert!(!mac.is_universal());
-    }
-
-    #[test]
-    fn test_display() {
-        let mac = MacAddr::new(0x00, 0x01, 0x02, 0x03, 0x04, 0x05);
-        let expect = "00:01:02:03:04:05";
-        let actual = mac.to_string();
-        assert_eq!(expect, actual);
     }
 }
